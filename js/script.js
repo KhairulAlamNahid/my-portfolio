a0ed84778ee7b71a02eaec1429b4381f2fa78d36
KhairulAlamNahid <profreelancer10@gmail.com>
2021-07-28 16:34:04 +0600

    fag

--- js/script.js
+++ js/script.js
@@ -83,10 +83,10 @@ $(function () {
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 576,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
