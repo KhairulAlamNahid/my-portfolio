b26962cd9073c92f43cf260a5508b36924f50562
KhairulAlamNahid <profreelancer10@gmail.com>
2021-07-28 17:05:05 +0600

    gsgs

--- js/script.js
+++ js/script.js
@@ -110,7 +110,7 @@ $(function () {
     var btn = $('#button');
 
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 100) {
+        if ($(window).scrollTop() > 200) {
             btn.addClass('show');
         } else {
             btn.removeClass('show');
